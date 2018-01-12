d9450d08afc24a5bce79d37782f045a849aa3788
mpizarrob20 <mpizarrob20@gmail.com>
2018-01-12 17:52:15 -0300

    Primer ejemplo

---
+++ script.js
@@ -0,0 +1,55 @@
+//Lecture variables
+/*
+console.log('Hello World!');
+console.log(age);
+
+*/
+var name = 'John';
+var lastName = 'Smith';
+var age = 26
+var fullAge = true;
+console.log(name+lastName);
+console.log(age);
+console.log(fullAge);
+console.log(age + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married ' + isMarried + '.');
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+//alert(name + ' is a ' + age + ' years old ' + job + '. Is he married ' + isMarried + '.');
+
+//Lecture: Operators
+
+var now = 2016
+var birthYear = now - 26
+
+birthYear = now - 26 * 2;
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+//ageJohn = ageMarck = 26
+
+ageJohn++;
+ageMark *= 2;
+ageMark = ageMark * 2;
+
+console.log(ageMark);
+console.log(ageJohn);
+
+
+
