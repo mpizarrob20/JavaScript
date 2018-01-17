824a98866f5f7275bf628658cf1aefea719a050a
mpizarrob20 <mpizarrob20@gmail.com>
2018-01-17 18:46:34 -0300

    Se adiciona un ejercicio practico y se actualiza el scriptBasic

--- scriptBasic.js
+++ scriptBasic.js
@@ -144,6 +144,188 @@ yearsUntilRetirement('Mike', 1969);
 yearsUntilRetirement('Mary', 1948);
 
 
+////////////////////////////////////////////////
+/////////Lecture : Statements and expressions
+
+function someFun(par){
+    //code
+}
+
+var someFun = function(par){
+    //code
+}
+
+//Expresions
+3 + 4;
+var x = 3;
+
+
+//Statements
+if(x === 5) {
+    //do something
+}
+
+
+/////////////////////////////////
+///// Lecture: Arrays
+
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names);
+console.log(names[2]);
+
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+console.log(john);
+
+john.unshift('Mr.');
+console.log(john);
+
+john.pop(); //delete last element
+console.log(john);
+
+john.shift();//delete first element
+console.log(john);
+
+//alert(john.indexOf('Smith'));
+if (john.indexOf('teacher') === -1){
+    console.log('John is NOT a teacher');
+}
+
+
+/////////////////////////////////////////
+//////Lecture: Objects
+
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName = 'Miller';
+john['job'] = 'programmer';
+
+console.log(john);
+
+// Object
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+
+
+////////////////////////////////////
+////Lecture: Objects and methods
+//v1.0
+/*var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(yearOfBirth){
+        return 2016 - this.yearOfBirth;
+    }
+};
+
+//console.log(john.calculateAge(1970));
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+
+console.log(john); */
+
+
+//v2.0
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(yearOfBirth){
+        this.age = 2018 - this.yearOfBirth;
+    }
+};
+
+john.calculateAge();
+console.log(john);
+
+var mike = {
+    yearOfBirth: 1950,
+    calculateAge: function(yearOfBirth){
+        this.age = 2018 - this.yearOfBirth;
+    }
+}
+
+mike.calculateAge();
+console.log(mike);
+
+/////////////////////////////////////////////
+/////Lecture: Loops
+
+//for loops
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+for (var i=0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+for(var i = names.length - 1; i >= 0; i --){
+    console.log(names[i]);
+}
+
+//while loops
+
+var i = 0;
+while(i < names.length){
+    console.log(names[i]);
+    i++;
+}
+
+
+for(var i = 1; i <= 5; i++){
+    console.log(i);
+    
+    if(i === 3){
+        break;
+    }
+}
+
+for(var i = 1; i <= 5; i++){    
+    if(i === 3){
+        continue;
+    }
+    
+    console.log(i);
+}
+
+
+
 
 
 
---
+++ scriptEjercicio2.js
@@ -0,0 +1,30 @@
+////////////////////////////
+//// SOLUTIONS
+
+var years = [2001, 1985, 1994, 2014, 1973];
+
+function printFullAge(years){
+    var ages = [];
+    var fullAges = [];
+    
+    for(var i = 0; i < years.length; i++){
+        ages[i] = 2018 - years[i];
+    }
+
+    for(i = 0; i < ages.length; i++){
+        if(ages[i] >= 18){
+            console.log('Person ' + (i + 1) + ' is ' + ages[i] + ' years old, and is of full age.'); 
+            fullAges.push(true);
+        } else{
+            console.log('Person ' + (i + 1) + ' is ' + ages[i] + ' years old, and is NOT of full age.');
+            fullAges.push(false);
+        }
+    }
+    
+    return fullAges;
+}
+
+var full_1 = printFullAge(years);
+var full_2 = printFullAge([2012, 1915, 1999]);
+
+
